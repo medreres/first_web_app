ba7bfe7ed6c9e4762b573b67d267af995575839d
Максим Пригода <90519417+medreres@users.noreply.github.com>
2022-05-27 12:03:20 +0300

    a

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from flask import *
 from flask_mail import *
 from random import *
+from helpers import apology
+from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
+import datetime
+
 
 app = Flask(__name__)
 
@@ -17,23 +21,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 mail = Mail(app)
 otp = randint(000000, 999999)
-
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
-def apology(message, code=400):
-    """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
 
 # homepage
 @app.route("/", methods=["GET"])
@@ -41,17 +30,84 @@ def homepage():
     return render_template("homepage.html")
 
 
+# contacts
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
+
+# timetable
+@app.route("/timetable")
+def timetable():
+    return render_template("timetable.html")
+
+
 # grades page
-@app.route("/grades")
+@app.route("/grades", methods=["GET", "POST"])
 def grades():
-    return apology("TODO")
+    con = sqlite3.connect('grades.db')
+    cursorObj = con.cursor()
+    if request.method == "GET":
+        if session["level"] == 0:
+            # math+, ukrainian, history, PE, informatics, english, history_ukraine
+            cursorObj.execute(
+                "SELECT * FROM grades WHERE student_id = ?", (session["user_id"],))
+            grades = cursorObj.fetchall()
+            math = []
+            ukrainian = []
+            history = []
+            informatics = []
+            english = []
+            history_ukraine = []
+            for grade in grades:
+                if grade[5] == "MATH":
+                    math.append(grade)
+                elif grade[5] == "UKRAINIAN":
+                    ukrainian.append(grade)
+                elif grade[5] == "HISTORY":
+                    history.append(grade)
+                elif grade[5] == "INFORMATICS":
+                    informatics.append(grade)
+                elif grade[5] == "ENGLISH":
+                    english.append(grade)
+                elif grade[5] == "HISTORY_UKRAINE":
+                    history_ukraine.append(grade)
+            return render_template("grades.html", math=math, ukrainian=ukrainian,
+                                   history=history, informatics=informatics, english=english,
+                                   history_ukraine=history_ukraine)
+        else:
+            cursorObj.execute(
+                "SELECT * FROM grades WHERE teacher_id = ?", (session["user_id"],))
+            grades = cursorObj.fetchall()
+            return render_template("grades.html", grades=grades)
+    else:
+        subject = request.form.get("subject")
+        print(subject)
+        if subject is None:
+            return apology("Subject hasn't been chosen")
+        grade = request.form.get("grade")
+        student_id = request.form.get("student_id")
+        date = datetime.datetime.now()
+        entities = (session["user_id"], int(student_id), int(
+            grade), date.strftime('%Y-%m-%d %H:%M:%S'), subject)
+        print(entities)
+        cursorObj.execute(
+            "INSERT INTO grades (teacher_id, student_id, grade, date, subject) VALUES (?,?,?,?,?)", entities)
+        con.commit()
+        return redirect("/grades")
+
 
 # profile page
-
-
 @app.route("/profile")
 def profile():
-    return apology("TODO")
+    status = "Error"
+    if session["level"] == 0:
+        status = "Student"
+    elif session["level"] == 1:
+        status = "Teacher"
+    elif session["level"] == 2:
+        status = "Admin"
+    return render_template("profile.html", status=status)
 
 
 # login page
@@ -60,7 +116,32 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
     else:
-        print("  ")
+        login = request.form.get("username")
+        password = request.form.get("password")
+        hashed_password = generate_password_hash(password)
+
+        # entering database
+        con = sqlite3.connect('users.db')
+        cursorObj = con.cursor()
+        cursorObj.execute(
+            "SELECT * FROM users WHERE username = ?", (login,))
+        user = cursorObj.fetchone()
+        if user is None:
+            cursorObj.execute(
+                "SELECT * FROM users WHERE email = ?", (login,))
+            user = cursorObj.fetchone()
+        if user is None:
+            return apology("User isn't found!")
+        if not check_password_hash(user[1], password):
+            return apology("Wrong password")
+        session["user_id"] = user[0]
+        session["year"] = user[7]
+        session["username"] = user[3]
+        session["first_name"] = user[2]
+        session["last_name"] = user[4]
+        session["email"] = user[5]
+        session["level"] = user[6]
+        flash(f"Hello, {user[2]}!")
         return redirect("/")
 
 
@@ -70,7 +151,7 @@ def logout():
 
     # Forget any user_id
     session.clear()
-
+    flash("Logged out!")
     # Redirect user to login form
     return redirect("/")
 
@@ -97,7 +178,10 @@ def register():
         # checking passwords for similarity
         if request.form.get("password") != request.form.get("password1"):
             return apology("Passowrds don't match")
-        session["password"] = request.form.get("password")
+        password = request.form.get("password")
+        hashed_password = generate_password_hash(password)
+        session["password"] = hashed_password
+        print(f"Hashed password to db {hashed_password}")
 
         # getting name
         session["first_name"] = request.form.get("first_name")
@@ -116,7 +200,7 @@ def register():
                       recipients=[session["email"]])
         msg.body = str(otp)
         mail.send(msg)
-    return render_template("/validate.html", code=otp, username=request.form.get("username"))
+        return render_template("/validate.html", code=otp, username=request.form.get("username"))
 
 
 # validating email
@@ -136,12 +220,18 @@ def validate():
             con.commit()
 
             # getting user's id
-            print(session["username"])
             cursorObj.execute(
-                "SELECT user_id FROM users WHERE username = ?", (session["username"],))
-            user_id = cursorObj.fetchall()
-            print(user_id)
-            session["user_id"] = user_id
+                "SELECT * FROM users WHERE username = ?", (session["username"],))
+            user = cursorObj.fetchone()
+            print(user)
+            session.clear()
+            session["user_id"] = user[0]
+            session["year"] = user[7]
+            session["username"] = user[3]
+            session["first_name"] = user[2]
+            session["last_name"] = user[4]
+            session["email"] = user[5]
+            session["level"] = user[6]
         else:
             session.clear()
             flash("Something went wrong!")
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app.main import app
+from app.helpers import apology
 
 if __name__ == "__main__":
     app.run(debug=True)
